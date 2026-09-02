aea186dcd8380292e6f8fd65ffcb531d9ff11548
Shubham Patil <shubhampatil9237@gmail.com>
2026-09-02 22:07:08 +0530

    Try some string in built funtions

--- C Module/Assignments/Assignment 10/StringInBuiltFunctions.c
+++ C Module/Assignments/Assignment 10/StringInBuiltFunctions.c
@@ -2,6 +2,13 @@
 #include<string.h>
 
 void main(){
-	char str[] = "Shubham";
-	printf("%s", str);	
+	char a[] = "Shubham";
+	char b[20];
+	printf("\nString a : %s", a);
+	printf("\nString length : %d", strlen(a));
+	printf("\nString a copy in b string: %s", strcpy(b, a));
+	char c[10];
+	strncpy(c, a, 3);
+	c[4] = '\0';
+	printf("\nFirst 3 char of string a copy in sting c: %s", c);
 }
